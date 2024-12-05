9a163932bf594c706efdf4bed7aec66188281e5d
deepzy1 <hdeepakranganath@gmail.com>
2024-12-05 15:18:54 +0530

    ,,

---
+++ models/property.py
@@ -0,0 +1,30 @@
+from odoo import fields,api,models
+
+class NewProperty(models.Model):
+    _name="new.property"
+    _description="New Property"
+
+    name=fields.Char(string="Name", help="Property Name", required=True)
+    type=fields.Many2one('asset.category', string="Type", required=True)
+    image_ids=fields.One2many('property.image', 'property_id', string="Images")
+    address=fields.Char(string="Address", required=True)
+    city=fields.Char(string="City", required=True)
+    pincode=fields.Char(string="Pincode", required=True)
+    district=fields.Char(string="District", required=True)
+    state=fields.Char(string="State", required=True)
+    price=fields.Float(string="Price", required=True)
+    document=fields.Binary(string="Document",required=True)
+
+
+
+class PropertyImage(models.Model):
+    _name="property.image"
+    _description=" Property Images"
+
+
+    image=fields.Binary(string="Image", attachment=True)
+    property_id=fields.Many2one('new.property', string="Property", ondelete="cascade")
+
+
+
+
