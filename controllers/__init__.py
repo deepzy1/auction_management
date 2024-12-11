3561f61fe0af367155b51ff6143f37da4485dcf3
Abhishekmh17 <abhishekmh1289@gmail.com>
2024-12-11 17:45:21 +0530

    added js and css

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,3 +1,1 @@
-from .import property_controller
-from . import user_login
-from . import new_property
+from . import home,user_register,properties
--- controllers/properties.py
+++ controllers/properties.py
@@ -13,4 +13,3 @@ class PropertyController(http.Controller):
 
 
 
-
